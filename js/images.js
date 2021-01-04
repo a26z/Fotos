54222e270ebed2c3b6827cb79926b7dccbb57c36
Christian Schmidt <christian@a26z.de>
2021-01-04 17:01:47 +0100

    module pics object

---
+++ js/images.js
@@ -0,0 +1,62 @@
+export const images = [
+    {
+        path: 'img/0.jpg',
+        alt: 'Dawn',
+        caption: 'Amanece a la salida del departamento.'
+    },
+    {
+        path: 'img/1.jpg',
+        alt: 'Bolígrafos',
+        caption: 'Vaciados en algo más de un año.'
+    },
+    {
+        path: 'img/2.jpg',
+        alt: 'Building',
+        caption: 'A shot of a building that looks like a vessel.'
+    },
+    {
+        path: 'img/3.jpg',
+        alt: 'Hafenstadt',
+        caption: 'Hinter diesem Gebäude eine unvorstellbare Maschine.'
+    },
+    {
+        path: 'img/4.jpg',
+        alt: 'Rose',
+        caption: 'Just a drinking flower.'
+    },
+    {
+        path: 'img/5.jpg',
+        alt: 'Marktplatz',
+        caption: 'Where japanese tourists take shots.'
+    },
+    {
+        path: 'img/6.jpg',
+        alt: 'Hansestadt',
+        caption: 'Natürlich im Norden des Landes.'
+    },
+    {
+        path: 'img/7.jpg',
+        alt: 'Trabant',
+        caption: 'Muy cerca de un lugar en donde residí.'
+    },
+    {
+        path: 'img/8.jpg',
+        alt: 'Tarde',
+        caption: 'And perhaps much earlier than one would think.'
+    },
+    {
+        path: 'img/9.jpg',
+        alt: 'Bar',
+        caption: 'En un puerto del otro hemisferio.'
+    },
+    {
+        path: 'img/10.jpg',
+        alt: 'Almuerzo',
+        caption: 'Barras apanadas de pescado con papas cocidas y ensalada de tomate con aceitunas.'
+    },
+    {
+        path: 'img/11.jpg',
+        alt: 'Tranvía',
+        caption: 'En la reciente actualidad.'
+    }
+]
